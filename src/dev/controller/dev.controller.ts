0d2972b2a4747bc3cb7bd6776482182c68b034ea
Leonard M Reidy <leonard.reidy@gmail.com>
2024-04-01 00:25:55 +0100

    DI fixes dev controller

--- src/dev/controller/dev.controller.ts
+++ src/dev/controller/dev.controller.ts
@@ -4,19 +4,20 @@ import {
   Route,
 } from 'tsoa';
 import { DevModel } from '../model/dev.model';
-import { injectable } from 'tsyringe';
+import { inject, injectable } from 'tsyringe';
 import { DevService } from '../service/dev.service';
 
 @injectable()
 @Route('dev')
 export class DevController extends Controller {
-  constructor(private devService: DevService) {
+  constructor(@inject('DevService') private devService: DevService) {
     super();
   }
 
-  @Get()
+  @Get('info')
   public async getDevInfo(): Promise<DevModel> {
-    return this.devService.getDevInfo();
+    return await this.devService.getDevInfo();
   }
+
 }
 
