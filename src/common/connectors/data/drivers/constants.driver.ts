16e3c5d713b49a9adc65cf9ad3c99a0d5dee4ce1
Leonard M Reidy <leonard.reidy@gmail.com>
2024-04-01 00:22:38 +0100

    Add constants

---
+++ src/common/connectors/data/drivers/constants.driver.ts
@@ -0,0 +1,7 @@
+import { DriverOptionsConfig } from './types';
+
+export const DefaultDriverConfig: DriverOptionsConfig = {
+  pass: 'neo4jdev',
+  uri: 'neo4j://localhost:7687',
+  user: 'neo4j',
+};
